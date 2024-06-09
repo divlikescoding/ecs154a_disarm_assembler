ce14869fbfefd15b53d88e828394fe32742e4596
Divyansh Jain <divyanshrajeshjain@gmail.com>
2024-06-09 11:26:31 -0700

    Added Listing output afte assembling in the format prof wants for the report

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,27 @@ extern "C" {
 
 #include <map>
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 #include <vector>
+#include <string>
 
-uint16_t parse_inst(inst_t *inst, uint16_t pc, std::map<std::string, uint16_t> symbol_table) {
+std::pair<uint16_t, std::string> parse_inst(inst_t *inst, uint16_t pc, std::map<std::string, uint16_t> symbol_table) {
     uint16_t assembled_inst;
+    std::string instruction;
     if (inst->inst_type == I_TYPE_NO_OPER) {
         if (inst->inst_u.no_oper->inst_type == I_NO_NOP) {
             assembled_inst = 0x0000;
+            instruction = "NOP";
         } else if (inst->inst_u.no_oper->inst_type == I_NO_RTI) {
             assembled_inst = 0x0200;
+            instruction = "RTI";
         } else if (inst->inst_u.no_oper->inst_type == I_NO_HALT) {
             assembled_inst = 0x0600;
+            instruction = "HALT";
         }
     } else if (inst->inst_type == I_TYPE_BRANCH) {
+        instruction = "B";
         if (symbol_table.find((char *) inst->inst_u.branch->label) == symbol_table.end()) {
             std::cerr << "Label " << inst->inst_u.branch->label << " is not defined." << std::endl;
             exit(0);
@@ -34,43 +42,65 @@ uint16_t parse_inst(inst_t *inst, uint16_t pc, std::map<std::string, uint16_t> s
             assembled_inst = 0x4000;
         } else if (inst->inst_u.branch->inst_type == I_BR_BEQ) {
             assembled_inst = 0x6000;
+            instruction += "EQ";
         } else if (inst->inst_u.branch->inst_type == I_BR_BCS) {
             assembled_inst = 0x7000;
+            instruction += "CS";
         }
+        std::string branch_label((char *) inst->inst_u.branch->label);
         assembled_inst += (abs_address - (pc + 1)) & 0x0FFF;
+        std::stringstream sstream;
+        sstream << std::setfill('0') << std::setw(3) << std::uppercase << std::hex << ((abs_address - (pc + 1)) & 0x0FFF);
+        instruction += " " + sstream.str();
     } else if (inst->inst_type == I_TYPE_ALU) {
         if (inst->inst_u.alu->inst_type == I_ALU_ADD) {
             assembled_inst = 0x0;
+            instruction = "ADD";
         } else if (inst->inst_u.alu->inst_type == I_ALU_SUB) {
             assembled_inst = 0x1;
+            instruction = "SUB";
         } else if (inst->inst_u.alu->inst_type == I_ALU_MUL) {
             assembled_inst = 0x2;
+            instruction = "MUL";
         } else if (inst->inst_u.alu->inst_type == I_ALU_MULU) {
             assembled_inst = 0x3;
+            instruction = "MULU";
         } else if (inst->inst_u.alu->inst_type == I_ALU_DIV) {
             assembled_inst = 0x4;
+            instruction = "DIV";
         } else if (inst->inst_u.alu->inst_type == I_ALU_MOD) {
             assembled_inst = 0x5;
+            instruction = "MOD";
         } else if (inst->inst_u.alu->inst_type == I_ALU_AND) {
             assembled_inst = 0x6;
+            instruction = "AND";
         } else if (inst->inst_u.alu->inst_type == I_ALU_OR) {
             assembled_inst = 0x7;
+            instruction = "OR";
         } else if (inst->inst_u.alu->inst_type == I_ALU_EOR) {
             assembled_inst = 0x8;
+            instruction = "EOR";
         } else if (inst->inst_u.alu->inst_type == I_ALU_NOT) {
             assembled_inst = 0x9;
+            instruction = "NOT";
         } else if (inst->inst_u.alu->inst_type == I_ALU_LSL) {
             assembled_inst = 0xA;
+            instruction = "LSL";
         } else if (inst->inst_u.alu->inst_type == I_ALU_LSR) {
             assembled_inst = 0xB;
+            instruction = "LSR";
         } else if (inst->inst_u.alu->inst_type == I_ALU_ASR) {
             assembled_inst = 0xC;
+            instruction = "ASR";
         } else if (inst->inst_u.alu->inst_type == I_ALU_ROL) {
             assembled_inst = 0xD;
+            instruction = "ROL";
         } else if (inst->inst_u.alu->inst_type == I_ALU_ROR) {
             assembled_inst = 0xE;
+            instruction = "ROR";
         } else if (inst->inst_u.alu->inst_type == I_ALU_CMP) {
             assembled_inst = 0xF;
+            instruction = "CMP";
         } 
         assembled_inst += 0x10;
         assembled_inst <<= 3;
@@ -79,20 +109,44 @@ uint16_t parse_inst(inst_t *inst, uint16_t pc, std::map<std::string, uint16_t> s
         assembled_inst += inst->inst_u.alu->rd;
         assembled_inst <<= 3;
         assembled_inst += inst->inst_u.alu->rm;
+        if (inst->inst_u.alu->inst_type == I_ALU_NOT) {
+            instruction += " R" + std::to_string(unsigned(inst->inst_u.alu->rd)) + ", R" + 
+                std::to_string(unsigned(inst->inst_u.alu->rn));
+        } else if (inst->inst_u.alu->inst_type == I_ALU_CMP) {
+            instruction += " R" + std::to_string(unsigned(inst->inst_u.alu->rn)) + ", R" + 
+                std::to_string(unsigned(inst->inst_u.alu->rm));
+        } else {
+            instruction += " R" + std::to_string(unsigned(inst->inst_u.alu->rd)) + ", R" + 
+                std::to_string(unsigned(inst->inst_u.alu->rn)) + ", R" + 
+                std::to_string(unsigned(inst->inst_u.alu->rm));
+        }
     } else if (inst->inst_type == I_TYPE_MEM) {
         if (inst->inst_u.mem->inst_type == I_MEM_LDR) {
             assembled_inst = 0x08;
+            instruction = "LDR";
         } else if (inst->inst_u.mem->inst_type == I_MEM_STR) {
             assembled_inst = 0x0C;
+            instruction = "STR";
         }
         if (inst->inst_u.mem->mem_mode == I_MEM_M_OFFSET) {
             assembled_inst += 0x0;
+            instruction += " R" + std::to_string(unsigned(inst->inst_u.mem->rd)) + ", [R" +
+                std::to_string(unsigned(inst->inst_u.mem->rn)) + ", R" +
+                std::to_string(unsigned(inst->inst_u.mem->rm)) + "]";
         } else if (inst->inst_u.mem->mem_mode == I_MEM_M_OFFSET_2) {
             assembled_inst += 0x3;
+            instruction += " R" + std::to_string(unsigned(inst->inst_u.mem->rd)) + ", [R" +
+                std::to_string(unsigned(inst->inst_u.mem->rn)) + "]";
         } else if (inst->inst_u.mem->mem_mode == I_MEM_M_PRE) {
             assembled_inst += 0x1;
+            instruction += " R" + std::to_string(unsigned(inst->inst_u.mem->rd)) + ", [R" +
+                std::to_string(unsigned(inst->inst_u.mem->rn)) + ", R" +
+                std::to_string(unsigned(inst->inst_u.mem->rm)) + "]!";
         } else if (inst->inst_u.mem->mem_mode == I_MEM_M_POST) {
             assembled_inst += 0x2;
+            instruction += " R" + std::to_string(unsigned(inst->inst_u.mem->rd)) + ", [R" +
+                std::to_string(unsigned(inst->inst_u.mem->rn)) + "], R" +
+                std::to_string(unsigned(inst->inst_u.mem->rm));
         }
         assembled_inst <<= 3;
         assembled_inst += inst->inst_u.mem->rn;
@@ -101,11 +155,16 @@ uint16_t parse_inst(inst_t *inst, uint16_t pc, std::map<std::string, uint16_t> s
         assembled_inst <<= 3;
         assembled_inst += inst->inst_u.mem->rm;
     } else if (inst->inst_type == I_TYPE_MOV) {
+        instruction = "MOV";
         if (inst->inst_u.mov->inst_type == I_MOV_IMM) {
             assembled_inst = 0x8;
             assembled_inst += inst->inst_u.mov->inst_u.imm->rd;
             assembled_inst <<= 12;
             assembled_inst += inst->inst_u.mov->inst_u.imm->imm & 0x0FFF;
+            std::stringstream sstream;
+            sstream << std::setfill('0') << std::setw(3) << std::uppercase << std::hex << unsigned(inst->inst_u.mov->inst_u.imm->imm & 0x0FFF);
+            instruction += " R" + std::to_string(unsigned(inst->inst_u.mov->inst_u.imm->rd)) + ", #0x"
+                + sstream.str();
         } else if (inst->inst_u.mov->inst_type == I_MOV_REG) {
             assembled_inst = 0x0;
             assembled_inst += inst->inst_u.mov->inst_u.reg->rn;
@@ -113,26 +172,30 @@ uint16_t parse_inst(inst_t *inst, uint16_t pc, std::map<std::string, uint16_t> s
             assembled_inst += inst->inst_u.mov->inst_u.reg->rd;
             assembled_inst <<= 3;
             assembled_inst += 0x0800;
+            instruction += " R" + std::to_string(unsigned(inst->inst_u.mov->inst_u.imm->rd)) + ", R"
+                + std::to_string(unsigned(inst->inst_u.mov->inst_u.reg->rn));
         } else if (inst->inst_u.mov->inst_type == I_MOV_FLAGS) {
             if (inst->inst_u.mov->inst_u.flags->is_flag_dest) {
                 assembled_inst = 0x0;
                 assembled_inst += inst->inst_u.mov->inst_u.flags->reg;
                 assembled_inst <<= 6;
                 assembled_inst += 0x0E00;
+                instruction += " Flags, R" + std::to_string(unsigned(inst->inst_u.mov->inst_u.flags->reg));
             } else {
                 assembled_inst = 0x0;
                 assembled_inst += inst->inst_u.mov->inst_u.flags->reg;
                 assembled_inst <<= 3;
                 assembled_inst += 0x0C00;
+                instruction += " R" + std::to_string(unsigned(inst->inst_u.mov->inst_u.flags->reg)) + ", Flags";
             }
         }
     }
-    return assembled_inst;
+    return std::make_pair(assembled_inst, instruction);
 }
 
-std::vector<uint16_t> parse_insts(inst_t *inst_head) {
+std::vector<std::pair<uint16_t, std::string> > parse_insts(inst_t *inst_head) {
     std::map<std::string, uint16_t> symbol_table;
-    std::vector<uint16_t> encoded_inst;
+    std::vector<std::pair<uint16_t, std::string> > encoded_inst;
     uint16_t index = 0;
 
     for (inst_t *curr_inst = inst_head; curr_inst != nullptr; curr_inst = curr_inst->next) {
@@ -153,7 +216,7 @@ std::vector<uint16_t> parse_insts(inst_t *inst_head) {
 
     index = 0; 
     for (inst_t *curr_inst = inst_head; curr_inst != nullptr; curr_inst = curr_inst->next) {
-        uint16_t parsed_inst = parse_inst(curr_inst, index, symbol_table);
+        std::pair<uint16_t, std::string> parsed_inst = parse_inst(curr_inst, index, symbol_table);
         encoded_inst.push_back(parsed_inst);
         index++;
     }
@@ -177,13 +240,15 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    std::vector<uint16_t> encoded_insts = parse_insts(inst_head);
+    std::vector<std::pair<uint16_t, std::string> > encoded_insts = parse_insts(inst_head);
 
     FILE* fout = fopen(argv[2], "w");
     fprintf(fout, "v2.0 raw");
-    for (std::vector<uint16_t>::iterator curr_elem = encoded_insts.begin(); curr_elem != encoded_insts.end(); curr_elem++) {
-        fprintf(fout, "\n%04jX", (uintmax_t) *curr_elem);
+    for (std::vector<std::pair<uint16_t, std::string> >::iterator curr_elem = encoded_insts.begin(); curr_elem != encoded_insts.end(); curr_elem++) {
+        fprintf(fout, "\n%04jX", (uintmax_t) curr_elem->first);
+        printf("\n%04jX: %s", (uintmax_t) curr_elem->first, curr_elem->second.c_str());
     }
+    printf("\n");
 
     return 0;
 }
